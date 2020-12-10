617f006bd2aa22eaf3d49acbf8d35b78e9c446c6
zhening.czn <zhening.czn@alibaba-inc.com>
2020-12-10 15:43:18 +0800

    update

--- foundation/src/main/java/me/yifeiyuan/foundation/Foundation.kt
+++ foundation/src/main/java/me/yifeiyuan/foundation/Foundation.kt
@@ -81,12 +81,15 @@ fun ViewGroup.inflate(resId: Int, attachToRoot: Boolean = true): View {
     return LayoutInflater.from(context).inflate(resId, this, attachToRoot)
 }
 
+/**
+ * 不可以调用 bitmap.recycle() 方法，有部分机子会导致 crash。
+ */
 fun ImageView.recycle() {
     try {
         (drawable as BitmapDrawable).let {
             setImageResource(0)
             setImageDrawable(null)
-            it.bitmap.recycle()
+//            it.bitmap.recycle()
             it.callback = null
         }
     } catch (e: Exception) {
